7530d944cd0b4422f105df27fc0df734a39fbe7d
mdrazaulkabir <razaulkabirit@gmail.com>
2025-12-18 20:32:18 +0600

    added progress api integration is done

--- lib/bottom_navigator_bar_all_screen/progress_screen.dart
+++ lib/bottom_navigator_bar_all_screen/progress_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:note_book_app/api_service/all_url.dart';
+import 'package:note_book_app/api_service/network_caller.dart';
+import 'package:note_book_app/custom_method/show_my_snack_bar.dart';
 import 'package:note_book_app/custom_widget/display_card.dart';
+import 'package:note_book_app/model/new_task_model.dart';
 
 class ProgressScreen extends StatefulWidget {
   const ProgressScreen({super.key});
@@ -9,14 +13,40 @@ class ProgressScreen extends StatefulWidget {
 }
 
 class _ProgressScreenState extends State<ProgressScreen> {
+  bool progressInProgress=false;
+  List<NewTaskModel>progressData=[];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _progressTaskList();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: ListView.builder(
-            itemCount: 20,
+            itemCount:progressData.length,
             itemBuilder: (context,index){
-              // return DisplayCard(textType: TextType.Progress,);
+              return DisplayCard(textType: TextType.Progress, newTaskModel: progressData[index],);
             })
     );
+  }
+
+  Future<void>_progressTaskList()async{
+    progressInProgress=true;
+    setState(() {});
+    NetworkResponse response=await NetworkCaller.getData(url: AllUrl.progressTaskListUrl);
+    if(response.isSuccess){
+      final List<NewTaskModel>listData=[];
+      for(Map<String,dynamic>listData1 in response.body!['data']){
+        listData.add(NewTaskModel.formJson(listData1));
+      }
+      progressData=listData;
+    }
+    else{
+      CMSnackBar(context, response.errorMessage!);
+    }
+    progressInProgress=false;
+    setState(() {});
   }
 }
