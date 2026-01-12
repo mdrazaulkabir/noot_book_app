088d19534b1451cfdbb55652efcc7279ed75c660
mdrazaulkabir <razaulkabirit@gmail.com>
2026-01-13 01:03:33 +0600

    set_password_screen api calling is done

--- lib/all_screen/set_password_screen.dart
+++ lib/all_screen/set_password_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:note_book_app/all_screen/signin_screen.dart';
+import 'package:note_book_app/api_service/all_url.dart';
+import 'package:note_book_app/api_service/network_caller.dart';
+import 'package:note_book_app/custom_method/show_my_snack_bar.dart';
 import 'package:note_book_app/custom_widget/rich_text1.dart';
+import 'package:note_book_app/model/email_verify_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SetPasswordScreen extends StatefulWidget {
   const SetPasswordScreen({super.key});
@@ -12,6 +18,7 @@ class _SetPasswordScreenState extends State<SetPasswordScreen> {
   final TextEditingController passTEController=TextEditingController();
   final TextEditingController confirmTEController=TextEditingController();
   final GlobalKey<FormState>_formKey=GlobalKey<FormState>();
+  bool setPasswordProgress=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,13 +70,13 @@ class _SetPasswordScreenState extends State<SetPasswordScreen> {
                   },
                 ),
                 SizedBox(height: 20),
-                // ElevatedButton(
-                //   onPressed: () {},
-                //   child: Text("Login"),
-                // ),
-                ElevatedButton.icon(onPressed: (){
-                  _verifyButton();
-                },label:Text("Confirm")),
+                Visibility(
+                  visible: setPasswordProgress==false,
+                  replacement: CMCircularProgress(),
+                  child: ElevatedButton.icon(onPressed: (){
+                    _verifyButton();
+                  },label:Text("Confirm")),
+                ),
                 SizedBox(height: 50),
                 RichText1(text1: "Have account?",text2: ' Sign in',),
               ],
@@ -82,11 +89,55 @@ class _SetPasswordScreenState extends State<SetPasswordScreen> {
   void _verifyButton(){
     if(_formKey.currentState!.validate()){
       //navigator
+      setPasswordApiCall();
+    }
+  }
+
+  Future<void>setPasswordApiCall()async{
+    setPasswordProgress=true;
+    setState(() {
+    });
+    SharedPreferences sh=await SharedPreferences.getInstance();
+    Map<String,dynamic>requestBody={
+      'email':sh.getString('email'),
+      'OTP':sh.getString('userOtp'),
+      'password':confirmTEController.text.trim()
+    };
+    NetworkResponse response =await NetworkCaller.postData(url: AllUrl.setPasswordUrl,body: requestBody,isLoggedIn: false);
+    if(response.statusCode==200){
+      EmailVerifyModel emailVerifyModel=EmailVerifyModel.fromJson(response.body!);
+      String? status1=emailVerifyModel.status;
+      String? data1=emailVerifyModel.data;
+      if(status1=='success'){
+        setPasswordProgress=false;
+       if(mounted){
+         passTEController.clear();
+         confirmTEController.clear();
+         CMSnackBar(context, "$status1 $data1");
+         //print('$status1 $data1'); //this is good practice because always you can't use context,,so just check in terminal by print ,,after check you can comment this
+         CMSnackBar(context, "now you can login your new password!");
+         Navigator.pushNamedAndRemoveUntil(context, SignInScreen.name, (route)=>false);
+       }
+      }
+      else{
+        setPasswordProgress=false;
+        setState(() {
+        });
+        //CMSnackBar(context, "$status1 $data1");
+        print("$status1 $data1");
+      }
+    }
+    else{
+      setPasswordProgress=false;
+      if(mounted){
+        setState(() {
+        });
+        CMSnackBar(context, response.errorMessage!);
+      }
     }
   }
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
     passTEController.dispose();
     confirmTEController.dispose();
--- lib/api_service/all_url.dart
+++ lib/api_service/all_url.dart
@@ -15,4 +15,5 @@ class AllUrl {
 
   static String emailVerifyUrl(String email)=>'$baseUrl/RecoverVerifyEmail/$email';
   static String pinOtpUrl(String email,String otp)=>'$baseUrl/RecoverVerifyOtp/$email/$otp';
+  static final String setPasswordUrl='$baseUrl/RecoverResetPassword';
 }
